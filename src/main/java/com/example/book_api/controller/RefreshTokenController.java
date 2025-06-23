93d307a5fe76efbe55a5ad24ab54c3ed1eefd51a
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-06-23 14:53:53 +0200

    ajout RefreshTokenController avec Post

--- src/main/java/com/example/book_api/controller/RefreshTokenController.java
+++ src/main/java/com/example/book_api/controller/RefreshTokenController.java
@@ -1,9 +1,25 @@
 package com.example.book_api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.book_api.model.RefreshToken;
+import com.example.book_api.service.RefreshTokenService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/refresh_token")
+@AllArgsConstructor
 public class RefreshTokenController {
+
+    //Injections de dépendances
+    private final RefreshTokenService refreshTokenService;
+
+    private record RefreshTokenRequest(String username) {}
+
+    @PostMapping("/generate_refresh_token")
+    public ResponseEntity<String> generate(@RequestBody RefreshTokenRequest refresh) {
+        String refreshToken = refreshTokenService.generateRefreshToken(refresh.username());
+        return ResponseEntity.ok(refreshToken);
+    }
+
 }
